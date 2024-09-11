460031f81fadf3dbc01ecf121a9c0e6df7683d55
Eren <yasar451278@gmail.com>
2024-09-11 20:25:39 +0300

    up

---
+++ main.js
@@ -0,0 +1,8 @@
+var camera1, camera2, scene1, scene2, renderer1, renderer2;
+
+var isUserInteracting=false,
+    onMouseDownMouseX = 0,
+    onMouseDownMouseY=0,
+    lon=0,
+    onMouseDownMouseLon=0,
+    
